b67a67e08768fb5cd205ad41f77934c3f0544cdf
David Barsky <barskydavid@gmail.com>
2015-12-21 11:59:13 -0500

    Initial Commit

---
+++ Hannibal/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Hannibal
+//
+//  Created by David Barsky on 12/21/15
+//  Copyright (c) 2015 David Barsky. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
