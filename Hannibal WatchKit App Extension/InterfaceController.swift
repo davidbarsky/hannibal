97c313f0c78e063972d3ab8bef8c0283fd2f5db6
David Barsky <barskydavid@gmail.com>
2015-12-21 12:34:24 -0500

    added watchkit to app

---
+++ Hannibal WatchKit App Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Hannibal WatchKit App Extension
+//
+//  Created by David Barsky on 12/21/15.
+//  Copyright © 2015 David Barsky. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
