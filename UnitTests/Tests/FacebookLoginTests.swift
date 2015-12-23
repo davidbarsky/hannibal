e12144ffd59d4d21b2b6b4f57e7651de6ac76fca
David Barsky <me@davidbarsky.com>
2015-12-23 13:31:32 -0500

    added login and main storyboards

---
+++ UnitTests/Tests/FacebookLoginTests.swift
@@ -0,0 +1,29 @@
+//
+//  FacebookLoginTests.swift
+//  Hannibal
+//
+//  Created by David Barsky on 12/23/15.
+//  Copyright © 2015 David Barsky. All rights reserved.
+//
+
+import XCTest
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class FacebookLoginTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testPresenceOfLoginToken() {
+        XCTAssertNotNil(FBSDKAccessToken.currentAccessToken(), "Current Access Token should no be nil")
+    }
+}
